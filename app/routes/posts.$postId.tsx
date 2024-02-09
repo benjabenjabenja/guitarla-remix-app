fc9529f6f0e77ab168935cb0b2acc70156dc05a4
benjamin gonzalez <jgonzalez.benjamin@gmail.com>
2024-02-09 15:15:47 -0300

    agregado links y meta al blog

--- app/routes/posts.$postId.tsx
+++ app/routes/posts.$postId.tsx
@@ -1,18 +1,9 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { IPostEntity, IPostParams } from "~/entities/posts.entitys";
-import { IApiResponse } from "~/entities/response-strappi.entitys";
+import { IApiResponse, ICollectionStrappi } from "~/entities/response-strappi.entitys";
 import { get_post_by_id } from "~/models/posts.server";
 import { format_date } from "~/utils/helpers.utils";
 import styles from '~/styles/blog.css';
-
-export function links() {
-    return [
-        {
-            rel: 'stylesheet',
-            href: styles
-        }
-    ]
-}
 
 export async function loader({ params }: { request: Request, params: IPostParams }): Promise<IApiResponse<IPostEntity>> {
     const { postId } = params;
@@ -26,10 +17,30 @@ export async function loader({ params }: { request: Request, params: IPostParams
     return response;
 }
 
+export function meta({ data }: { data: IApiResponse<IPostEntity>}) {
+    const { data: post } = data || { data: null };
+    const response = data ? [
+        { title: post[0]?.attributes.title + " - RemixRun App" },
+        { name: "description", content: "Marketplace of guitarrs, Guitarrs, Sale Guitar" },
+    ] :  [
+        { title: 'ERROR - RemixRun App' },
+        { name: 'description', content: 'Guitar not found' }
+    ];
+    return response;
+}
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ];
+}
+
 const PostId = () => {
     const { data } = useLoaderData() as IApiResponse<IPostEntity>;
     const { attributes } = data[0];
-    const { contenido, image, title, createdAt,url } = attributes;
+    const { contenido, image, title, createdAt } = attributes;
     return (
         <article className="contenedor post">
             <img className='imagen mt__5' src={image?.data?.attributes?.url} alt={`blog ${image.data.attributes.url}`} />
@@ -37,7 +48,7 @@ const PostId = () => {
                 <h3 className="heading"> { title } </h3>
                 <p className="fecha">{ format_date(createdAt) }</p>
                 {
-                    contenido && contenido.map( content => <p key={url} className="texto">{content?.children[0].text}</p>)
+                    contenido && contenido.map( (content, i) => <p key={i} className="texto">{content?.children[0].text}</p>)
                 }
                 <Link className="link" to="/blog"> back to blog</Link>
             </div>
